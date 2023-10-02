b9d10d19dc6b2db83432f7da8f5354db06e27da3
JustInCase289 <bishopjustin469@gmail.com>
2023-10-02 11:14:29 -0400

    Updated Code with accuracy counter

--- MainProd/HaarCascade/HaarImage.py
+++ MainProd/HaarCascade/HaarImage.py
@@ -6,24 +6,18 @@ eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 profile_cascade = cv2.CascadeClassifier('haarcascade_profileface.xml')
 
 # Load an image from file
-img = cv2.imread('C:\\School\\599\\StockImageGroup.jpg')
+img = cv2.imread('C:\\School\\599\\StockImageGroup2.jpg')
 
 # Convert to gray scale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Detect faces
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-profile_faces = profile_cascade.detectMultiScale(gray, 1.3, 5)
+
 # Detect faces with different minNeighbors
 faces_3n = face_cascade.detectMultiScale(gray, 1.3, 3)
 faces_5n = face_cascade.detectMultiScale(gray, 1.3, 5)
 faces_7n = face_cascade.detectMultiScale(gray, 1.3, 7)
 
-
-# Combine faces and profile faces detections
-all_faces = list(faces) + list(profile_faces)
-
-for (x, y, w, h) in all_faces:
+for (x, y, w, h) in faces_3n:
     # Blur the face region
     face_region = img[y:y + h, x:x + w]
     blurred_face = cv2.GaussianBlur(face_region, (99, 99), 30)
@@ -41,7 +35,7 @@ for (x, y, w, h) in all_faces:
     cv2.putText(img, confidence_text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
     # If you want to detect and blur eyes within frontal faces, do that within this loop
-    if (x, y, w, h) in faces:
+    if (x, y, w, h) in faces_3n:
         roi_gray = gray[y:y + h, x:x + w]
         roi_color = img[y:y + h, x:x + w]
         eyes = eye_cascade.detectMultiScale(roi_gray)
