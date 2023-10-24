6e190e0d17572924dd431b0c8fec0cdc179bf46a
JustInCase289 <bishopjustin469@gmail.com>
2023-10-24 11:00:15 -0400

    Added machine learning algorithm plus new folder containing 4 images that can be used to test.

--- MainProd/NeuralNetwork/Neural1.py
+++ MainProd/NeuralNetwork/Neural1.py
@@ -1,33 +1,36 @@
 import cv2
-import torch
-from torchvision.models.detection import fasterrcnn_resnet50_fpn
-from torchvision.transforms import functional as F
+from mtcnn import MTCNN
 
-# Load pre-trained model
-model = fasterrcnn_resnet50_fpn(pretrained=True).eval()
 
-# Load image
-image = cv2.imread('C:\\School\\599\\StockImages\\StockImageGroup3.jpg')
-image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+def display_blurred_faces(filename):
+    # Load image using OpenCV
+    image = cv2.imread("C:\\Users\\bisho\\PycharmProjects\\FacialRecog\\MainProd\\TestImages\\StockImageGroup4.jpg")
 
-# Convert to PyTorch tensor
-tensor_img = F.to_tensor(image_rgb).unsqueeze(0)
+    # Convert the image from BGR to RGB format (as MTCNN expects RGB)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-# Get predictions
-with torch.no_grad():
-    prediction = model(tensor_img)
+    # Initialize MTCNN detector
+    detector = MTCNN()
 
-# Extract bounding boxes
-boxes = prediction[0]['boxes']
+    # Detect faces in the image
+    faces = detector.detect_faces(image_rgb)
 
-for box in boxes:
-    x1, y1, x2, y2 = map(int, box)
-    # Extract the face from the image
-    face = image[y1:y2, x1:x2]
-    # Blur the face
-    blurred_face = cv2.GaussianBlur(face, (99, 99), 30)
-    # Replace original face with blurred one
-    image[y1:y2, x1:x2] = blurred_face
+    # For each detected face
+    for face in faces:
+        # Get coordinates and dimensions of the bounding box
+        x, y, width, height = face['box']
+        # Extract the face from the image
+        face_image = image[y:y + height, x:x + width]
+        # Blur the face
+        face_image = cv2.GaussianBlur(face_image, (99, 99), 30)
+        # Replace the original face with the blurred version
+        image[y:y + height, x:x + width] = face_image
 
-# Save or display the image
-cv2.imshow('output.jpg', image)
+    # Display the resulting image
+    cv2.imshow('Blurred Faces', image)
+    cv2.waitKey(0)  # Wait until a key is pressed
+    cv2.destroyAllWindows()  # Close the window
+
+
+# Example usage
+display_blurred_faces('input_image.jpg')
